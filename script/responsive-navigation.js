42508c9508e3a950029fd8bf9366bb0340eef387
Matheus Donangelo <mldonangelo@gmail.com>
2023-08-30 14:04:52 -0300

    space fix

--- script/responsive-navigation.js
+++ script/responsive-navigation.js
@@ -3,9 +3,20 @@ let hamburguerMenu = document.querySelector('.hamburguer-menu');
 let closeMenu = document.querySelector('.close-menu');
 let navContainer = document.querySelector('.nav-container');
 
-// Botões para abrir e fechar menu
+// Botões para abrir e fechar menu com clique
 hamburguerMenu.addEventListener("click", (e) => {toggleResponsiveMenu(e)});
 closeMenu.addEventListener("click", (e) => {toggleResponsiveMenu(e)});
+// Botões para abrir e fechar menu com espaço
+hamburguerMenu.addEventListener('keyup', e => {
+    if (event.code === 'Space') {
+        toggleResponsiveMenu(e)
+    }
+});
+closeMenu.addEventListener('keyup', e => {
+    if (event.code === 'Space') {
+        toggleResponsiveMenu(e)
+    }
+});
 
 // Fechar menu se usuário clicar fora do menu
 navContainer.addEventListener("click", function(e) {
