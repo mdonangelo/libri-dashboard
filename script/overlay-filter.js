53d9e2348f2f3014c94b099d0d68af669b8d851a
Matheus Donangelo <mldonangelo@gmail.com>
2023-08-31 10:55:51 -0300

    adjust script overlay

--- script/overlay-filter.js
+++ script/overlay-filter.js
@@ -1,39 +1,42 @@
 // Função para exibir ou fechar o overlay com todos os botões de filtro do cartão
-function showOrCloseFilter() {
-    let overlayDoFiltro = document.getElementById("overlay-filter");
+function showOrCloseFilter(idFilter) {
+    let overlaySelected = document.getElementById(idFilter);
     let body = document.body;
     
-    // Verificar se modal está aberto ou não
-    if(!overlayDoFiltro.classList.contains("display_none")) {
-        overlayDoFiltro.classList.add("display_none");
-        overlayDoFiltro.setAttribute('aria-hidden', "true");
-        overlayDoFiltro.setAttribute('aria-disabled', "true");
-        body.setAttribute('aria-hidden', "false");
-        body.setAttribute('aria-disabled', "false");
-    }else {
-        overlayDoFiltro.classList.remove("display_none");
-        overlayDoFiltro.setAttribute('aria-hidden', "false");
-        overlayDoFiltro.setAttribute('aria-disabled', "false");
-        overlayDoFiltro.setAttribute('tabindex', '0');
-        overlayDoFiltro.focus();
-        body.setAttribute('aria-hidden', "true");
-        body.setAttribute('aria-disabled', "true");
-        body.setAttribute('tabindex', '-1');
-    }
+    if(overlaySelected){
+        // Verificar se modal está aberto ou não
+        if(!overlaySelected.classList.contains("display_none")) {
+            overlaySelected.classList.add("display_none");
+            overlaySelected.setAttribute('aria-hidden', "true");
+            overlaySelected.setAttribute('aria-disabled', "true");
+            body.setAttribute('aria-hidden', "false");
+            body.setAttribute('aria-disabled', "false");
+        }else {
+            overlaySelected.classList.remove("display_none");
+            overlaySelected.setAttribute('aria-hidden', "false");
+            overlaySelected.setAttribute('aria-disabled', "false");
+            overlaySelected.setAttribute('tabindex', '0');
+            overlaySelected.focus();
+            body.setAttribute('aria-hidden', "true");
+            body.setAttribute('aria-disabled', "true");
+            body.setAttribute('tabindex', '-1');
+        }
 
-    // Uso de teclado esc para fechar modal
-    document.onkeydown = function(event) {
-        event = event || window.event;
-        if (event.code == "Escape" || event.code == "Enter") {
-            if(!overlayDoFiltro.classList.contains("display_none"))
-            showOrCloseFilter();
+        // Uso de teclado esc para fechar modal
+        document.onkeydown = function(event) {
+            event = event || window.event;
+            if (event.code == "Escape" || event.code == "Enter") {
+                if(!overlaySelected.classList.contains("display_none"))
+                showOrCloseFilter(idFilter);
+            }
+        }
+
+        // Clicar no outline para fechar modal
+        document.getElementById('overlay-filter').onclick = function(event) {
+            if(event.target == document.getElementById('overlay-filter')){
+                showOrCloseFilter(idFilter);
+            }
         }
     }
-
-    // Clicar no outline para fechar modal
-    document.getElementById('overlay-filter').onclick = function(event) {
-        if(event.target == document.getElementById('overlay-filter')){
-            showOrCloseFilter();
-        }
-    }
+    
 }
